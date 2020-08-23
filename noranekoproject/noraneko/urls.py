5fab3525bcb3a37651b25ad9bc5f6a32a9e4a1f2
MAYU MIYAUCHI <m0513mapple@yahoo.co.jp>
2020-08-23 13:44:50 +0900

    8/23

--- noranekoproject/noraneko/urls.py
+++ noranekoproject/noraneko/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from .views import indexfunc,list_chatfunc,create_chatfunc,detail_chatfunc
+from .views import indexfunc,list_chatfunc,detail_chatfunc,CreateChat
 
 urlpatterns = [
     path('index/',indexfunc, name='index' ),
     path('list_chat/',list_chatfunc, name='list_chat'),
-    path('create_chat/',create_chat.as_view(), name='create_chat'),
+    path('create_chat/',CreateChat.as_view(), name='create_chat'),
     path('detail_chat/',detail_chatfunc, name='detail_chat'),
 
 ]
--- noranekoproject/noraneko/views.py
+++ noranekoproject/noraneko/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import Create_chat
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from .models import NekoModel
 
 
 # Create your views here.
@@ -17,9 +16,11 @@ def detail_chatfunc(request):
   object = NekoModel.objects.get(pk=pk)
   return render(request,'detail_chat.html',{'object':object})
 
-class Create_chat(CreateView):
+class CreateChat(CreateView):
   template_name = 'create_chat.html'
-  model = Create_chat
   fields = ('title','content')
-  succsess_url = reverse_lazy('list_chat')
+  model = NekoModel
+  success_url = reverse_lazy('list_chat')
+
+
 
